413595972f439dc07cc19b7bdcc626fe0d3b81f0
Jan Ouwens <jan.ouwens@nummulus.com>
2013-03-19 20:45:52 +0100

    Added documentation

--- avond2-mutual-exclusion/exercise14/Exercise2.14/src/MultiFilter.java
+++ avond2-mutual-exclusion/exercise14/Exercise2.14/src/MultiFilter.java
@@ -1,4 +1,9 @@
 
+/**
+ * This class is a dumb copy of the Filter class, with some modifications
+ * to turn it into an ℓ-exclusion algorithm. The differences are explained
+ * in the comments.
+ */
 public class MultiFilter extends Lock {
 	final int N;
 	final int ℓ;
@@ -6,6 +11,7 @@ public class MultiFilter extends Lock {
 	volatile int[] level;
 	volatile int[] victim;
 	
+	// Added ℓ parameter. Pretty straight-forward, right?
 	public MultiFilter(int N, int ℓ) {
 		this.N = N;
 		this.ℓ = ℓ;
@@ -19,6 +25,9 @@ public class MultiFilter extends Lock {
 	@Override
 	public void lock(int threadId) {
 		int me = threadId;
+		// The Filter lock allows 1 simultaneous threads in the critical section.
+		// This algorithm should allow ℓ, so we need ℓ-1 less waiting rooms.
+		// Therefore, we can bound on N-(ℓ-1) = N-ℓ+1.
 		for (int i = 1; i < N-ℓ+1; i++) {
 			level[me] = i;
 			sleep();
@@ -29,11 +38,14 @@ public class MultiFilter extends Lock {
 	}
 	
 	private boolean exist(int me, int i) {
-		int num = 0;
+		// x is the number of other threads which have an equal or higher level than "me",
+		// where level[me] = i.
+		int x = 0;
 		for (int k = 0; k < N; k++) {
 			if (k != me && level[k] >= i) {
-				num++;
-				if (num == ℓ) {
+				// Search ℓ other thread that have equal or higher level.
+				x++;
+				if (x == ℓ) {
 					return true;
 				}
 			}
